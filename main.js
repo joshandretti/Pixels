8040c1b38192ec2ff6c67394437d48c61c7169d9
Josh <josh22henry@gmail.com>
2022-01-26 13:34:30 -0500

    update

--- main.js
+++ main.js
@@ -11,3 +11,27 @@ var swiper = new Swiper(".home", {
     },
     
   });
+
+  var swiper = new Swiper(".coming-container", {
+    spaceBetween: 20,
+    loop:true,
+    autoplay: {
+      delay: 55000,
+      disableOnInteraction: false,
+    },
+    centeredSlides: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 2,
+      },
+      568: {
+        slidesPerView: 3,
+      },
+      768: {
+        slidesPerView: 4,
+      },
+      968: {
+        slidesPerView: 5,
+      },
+    },
+  });
